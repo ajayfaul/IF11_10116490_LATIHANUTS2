c97e9a4726be1799c8365faf0de51b01f9e06b76
ajayfaul <ajayfaul@ajayfaul-desktop>
2017-11-14 16:49:09 +0700

    Gaji Pegawai

---
+++ src/model/GajiPegawai.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ajayfaul
+ * Kelas :IF-11
+ * 10116490 / Muhammad Fajar Maulana
+ */
+  
+public class GajiPegawai {
+    private String nama, alamat;
+    private int uangTransport, uangTunjangan, gajiPokok, totalGaji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getUangTransport() {
+        return uangTransport;
+    }
+
+    public void setUangTransport(int uangTransport) {
+        this.uangTransport = uangTransport;
+    }
+
+    public int getUangTunjangan() {
+        return uangTunjangan;
+    }
+
+    public void setUangTunjangan(int uangTunjangan) {
+        this.uangTunjangan = uangTunjangan;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(int gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
+
+    public int getTotalGaji() {
+        return totalGaji;
+    }
+    
+   
+
+    public int totalGaji(int uangTunjangan, int uangTransport, int gajiPokok){
+        
+       totalGaji=uangTunjangan + uangTransport + gajiPokok;
+       return totalGaji;
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
